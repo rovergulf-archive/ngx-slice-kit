36dc883cd7a17c2accaff1c8d0df5391c18a8974
Dmitrii Limonov <disordervlz@gmail.com>
2021-02-05 13:12:23 +0300

    remove button-group module button import and export

--- libs/ngx-slice-kit/src/lib/buttons/button-group/button-group.module.ts
+++ libs/ngx-slice-kit/src/lib/buttons/button-group/button-group.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonGroupComponent } from './button-group.component';
-import { ButtonComponent } from '../button/button.component';
-
 
 @NgModule({
     declarations: [
-        ButtonComponent,
         ButtonGroupComponent
     ],
     exports: [
-        ButtonComponent,
         ButtonGroupComponent
     ],
     imports: [
