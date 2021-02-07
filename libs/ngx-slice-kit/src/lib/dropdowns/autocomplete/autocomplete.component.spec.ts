6c05ec2779216a751b8612f26d1967c9f8e1779f
cosmicintruder <littlegreenbagg@googlemail.com>
2021-02-07 22:45:35 +0300

    test/dropdowns: autocomplete - add tests for multi prop as true and false;

--- libs/ngx-slice-kit/src/lib/dropdowns/autocomplete/autocomplete.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/dropdowns/autocomplete/autocomplete.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AutocompleteComponent } from './autocomplete.component';
-import {SelectComponent} from '../select/select.component';
 import {OPTIONS1} from '../../../../../../src/app/shared/values/dropdowns.values';
 import {PLATFORM_ID} from '@angular/core';
 import {OptionsService} from '../options.service';
 import {DropdownService} from '../dropdown.service';
+import {Observable, of} from 'rxjs';
+import {DropdownOptions} from '../dropdown.model';
 
 describe('AutocompleteComponent', () => {
     let component: AutocompleteComponent;
@@ -47,6 +48,161 @@ describe('AutocompleteComponent', () => {
         it('should create', () => {
             expect(component).toBeTruthy();
         });
+
+        it('should stop #showDropdown() fn if component already is opened', () => {
+            component.isOpen = true;
+            const showDropdownResult = component.showDropdown();
+
+            expect(showDropdownResult).toBeUndefined('if #isOpen is true dropdown should not be open again');
+        });
+
+        // it('should label be displayed if #label property is set', () => {
+        //     const labelEl = autocompleteEl.querySelector('.sdk-select-label');
+        //     let text = labelEl.textContent.trim();
+        //     const textDefault = '';
+        //     const textStub = 'Some text';
+        //     expect(text).toBe(textDefault);
+        //
+        //     component.label = textStub;
+        //     fixture.detectChanges();
+        //     text = labelEl.textContent.trim();
+        //     expect(text).toBe(textStub);
+        // });
+
+        /*
+         *  Tests with different component.multi value;
+        */
+
+        describe('with component.multi as "true"', () => {
+            beforeEach(() => {
+                component.multi = true;
+                component.ngOnInit();
+            });
+
+            it('should ngOnInit set #enableNullValue as "true"', () => {
+                expect(component.enableNullValue).toBeTrue();
+            });
+
+            it('should #currentValues be empty after ngOnInit', () => {
+                expect(component.currentValues.size).toBe(  0);
+            });
+
+            it('should #isInactive() return "true" if currentValues does not have elements', () => {
+                console.log(component.isInactive(), 'yoyoy', component.multi, component.currentValues)
+                expect(component.isInactive()).toBeTrue();
+            });
+
+            it('should #isInactive() return "false" if currentValues has elements', () => {
+                component.currentValues.add(stubOptionA);
+                expect(component.isInactive()).toBeFalse();
+            });
+
+            it('should #getOptions() return options with correct selected property', () => {
+                component.options = OPTIONS1;
+                const expectedOptionsList = [...OPTIONS1].map((el, index) => {
+                    el.selected = index === testIndexA || index === testIndexB;
+                    return el;
+                });
+                component.currentValues.add(stubOptionA);
+                component.currentValues.add(stubOptionB);
+
+                fixture.detectChanges();
+                expect(component.getOptions()).toEqual(expectedOptionsList);
+            });
+
+            it('should #selected() return options labels as join in string', () => {
+                const expectedSelectedString = `${stubOptionA.label}, ${stubOptionB.label}`;
+                component.currentValues.add(stubOptionA);
+                component.currentValues.add(stubOptionB);
+                fixture.detectChanges();
+
+                expect(component.selected()).toBe(expectedSelectedString);
+            });
+
+            it('should #onResult() call #addValue() and pass option', () => {
+                spyOn(component, 'addValue');
+                spyOn(component, 'writeValue');
+                component.onResult(stubOptionA);
+
+                expect(component.addValue).toHaveBeenCalledWith(stubOptionA);
+                expect(component.writeValue).not.toHaveBeenCalled();
+            });
+
+            it('should #clearValue() should reset #currentValues', () => {
+                component.currentValues.add(stubOptionA);
+                component.currentValues.add(stubOptionB);
+                fixture.detectChanges();
+                expect(component.currentValues.size).toBe(2);
+
+                component.clearValue(new MouseEvent('click'));
+                fixture.detectChanges();
+                expect(component.currentValues.size).toBe(0);
+            });
+
+            it('should #clearValue() emit #valueChanges event with null as argument', () => {
+                component.valueChanges.subscribe(res => {
+                    expect(res).toBeNull();
+                });
+
+                component.clearValue(new MouseEvent('click'));
+            });
+        });
+
+        describe('with component.multi as "false"', () => {
+            beforeEach(() => {
+                component.multi = false;
+                component.ngOnInit();
+            });
+
+            it('should #isInactive() return "true" if currentValue does not have value', () => {
+                expect(component.isInactive()).toBeTrue();
+            });
+
+            it('should #isInactive() return "false" if currentValue has value', () => {
+                component.currentValue = stubOptionA;
+                expect(component.isInactive()).toBeFalse();
+            });
+
+            it('should #getOptions() return options with correct selected property', () => {
+                component.options = OPTIONS1;
+                const expectedOptionsList = [...OPTIONS1].map((el, index) => {
+                    el.selected = index === testIndexA;
+                    return el;
+                });
+                component.currentValue = stubOptionA;
+
+                fixture.detectChanges();
+                expect(component.getOptions()).toEqual(expectedOptionsList);
+            });
+
+            it('should #selected() return #currentValue.label', () => {
+                const option = stubOptionA;
+                const expectedSelectedString = `${option.label}`;
+                component.currentValue = option;
+                fixture.detectChanges();
+
+                expect(component.selected()).toBe(expectedSelectedString);
+            });
+
+            it('should #onResult() call #writeValue() and pass option', () => {
+                spyOn(component, 'addValue');
+                spyOn(component, 'writeValue');
+                component.onResult(stubOptionA);
+
+                expect(component.writeValue).toHaveBeenCalledWith(stubOptionA);
+                expect(component.addValue).not.toHaveBeenCalled();
+            });
+
+            it('should #clearValue() should reset #currentValue', () => {
+                component.currentValue = stubOptionA;
+                fixture.detectChanges();
+                expect(component.currentValue).toEqual(stubOptionA);
+
+                component.clearValue(new MouseEvent('click'));
+                fixture.detectChanges();
+                expect(component.currentValue).toBeUndefined();
+            });
+        });
     });
 
     describe('with PLATFORM_ID as server', () => {
@@ -68,6 +224,67 @@ describe('AutocompleteComponent', () => {
             const result = component.showDropdown();
 
             expect(result).toBeUndefined('should be undefined if PLATFORM_ID is server');
+        });
+    });
+
+    describe('Test with dropdownService', () => {
+        let dropdownService;
+
+        beforeEach(() => {
+            TestBed.overrideProvider(DropdownService, {
+                useValue: {
+                    showDropdown: () => stubOptionA as Observable<any>
+                }
+            });
+            fixture = TestBed.createComponent(AutocompleteComponent);
+            component = fixture.componentInstance;
+            dropdownService = TestBed.inject(DropdownService);
+            fixture.detectChanges();
+
+            TestBed.compileComponents();
+        });
+
+        beforeEach(() => {
+            component.isOpen = false;
+            component.options = OPTIONS1;
+        });
+
+        it('should #showDropdown() call #onClose() after get date', () => {
+            spyOn(component, 'onClose');
+            spyOn(dropdownService, 'showDropdown').and.returnValue(of(stubOptionA));
+
+            fixture.detectChanges();
+            component.showDropdown();
+            expect(component.onClose).toHaveBeenCalled();
+        });
+
+        it('should #showDropdown() call #onResult() with data argument after get it', () => {
+            spyOn(dropdownService, 'showDropdown').and.returnValue(of(stubOptionA));
+            spyOn(component, 'onResult');
+
+            const opts: DropdownOptions = {
+                triggerRect: component.autocomplete.nativeElement.getBoundingClientRect(),
+                fitWidth: true,
+                hideBackdrop: true,
+                multi: component.multi,
+                parentElem: component.autocomplete.nativeElement,
+            };
+
+            fixture.detectChanges();
+            component.showDropdown();
+
+            expect(dropdownService.showDropdown).toHaveBeenCalledWith(opts);
+            expect(component.onResult).toHaveBeenCalledWith(stubOptionA);
+        });
+
+        it('should #showDropdown() not call #onResult() if have no data', () => {
+            spyOn(dropdownService, 'showDropdown').and.returnValue(of(undefined));
+            spyOn(component, 'onResult');
+
+            fixture.detectChanges();
+            component.showDropdown();
+
+            expect(component.onResult).not.toHaveBeenCalled();
         });
     });
 });
