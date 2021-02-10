95a327946cd0a115e03f2da07e5732498630d471
cosmicintruder <littlegreenbagg@googlemail.com>
2021-02-11 00:29:02 +0300

    test/sidenav - init;

--- libs/ngx-slice-kit/src/lib/navigation/sidenav/sidenav.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/navigation/sidenav/sidenav.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SidenavComponent } from './sidenav.component';
+import {DebugElement} from '@angular/core';
 
 describe('SidenavComponent', () => {
     let component: SidenavComponent;
     let fixture: ComponentFixture<SidenavComponent>;
+    let sidenavDe: DebugElement;
+    let sidenavEl: HTMLElement;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
@@ -16,6 +19,8 @@ describe('SidenavComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(SidenavComponent);
         component = fixture.componentInstance;
+        sidenavDe = fixture.debugElement;
+        sidenavEl = sidenavDe.nativeElement;
         fixture.detectChanges();
     });
 
