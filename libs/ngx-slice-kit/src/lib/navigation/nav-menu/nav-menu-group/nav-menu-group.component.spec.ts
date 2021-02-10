2a8df6b4102ec8381df72d57de0ce04d56ab2cc9
cosmicintruder <littlegreenbagg@googlemail.com>
2021-02-11 00:20:37 +0300

    test/navigation: nav-menu - nav-menu-group done;

--- libs/ngx-slice-kit/src/lib/navigation/nav-menu/nav-menu-group/nav-menu-group.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/navigation/nav-menu/nav-menu-group/nav-menu-group.component.spec.ts
@@ -3,11 +3,12 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NavMenuGroupComponent } from './nav-menu-group.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DebugElement} from '@angular/core';
+import {Component, DebugElement} from '@angular/core';
+import {NavMenuItemComponent} from '../nav-menu-item/nav-menu-item.component';
 
 describe('NavMenuGroupComponent', () => {
     let component: NavMenuGroupComponent;
-    let fixture: ComponentFixture<NavMenuGroupComponent>;
+    let fixture: ComponentFixture<TestComponent>;
     let navGroupDe: DebugElement;
     let navGroupEl: HTMLElement;
 
@@ -17,14 +18,18 @@ describe('NavMenuGroupComponent', () => {
                 RouterTestingModule,
                 BrowserAnimationsModule
             ],
-            declarations: [NavMenuGroupComponent]
+            declarations: [
+                NavMenuGroupComponent,
+                NavMenuItemComponent,
+                TestComponent,
+            ]
         })
             .compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(NavMenuGroupComponent);
-        component = fixture.componentInstance;
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.debugElement.children[0].componentInstance;
         navGroupDe = fixture.debugElement;
         navGroupEl = navGroupDe.nativeElement;
         fixture.detectChanges();
@@ -69,4 +74,53 @@ describe('NavMenuGroupComponent', () => {
         fixture.detectChanges();
         expect(titleEl.textContent).toBe(label);
     });
+
+    it('should correct src set component active', () => {
+        component.ngAfterContentInit();
+        component.checkIsActive('/guides/fantastic');
+        fixture.detectChanges();
+
+        expect(component.isActive).toBeTrue();
+        expect(component.isOpen).toBeTrue();
+    });
+
+    it('should invalid src set component inactive', () => {
+        component.ngAfterContentInit();
+        component.checkIsActive('/guides/doom');
+        fixture.detectChanges();
+
+        expect(component.isActive).toBeFalse();
+        expect(component.isOpen).toBeFalse(); // because isOpen is false by default
+    });
+
+    it('should #deepLvl be equal 1 if it is not inner subgroup', () => {
+        component.ngAfterContentInit();
+
+        expect(component.deepLvl).toBe(1);
+    });
+
+    it('should .nav-menu-group__items has correct #deeplvl attribute', () => {
+        const el: HTMLElement = navGroupEl.querySelector('.nav-menu-group__items');
+        component.ngAfterContentInit();
+        fixture.detectChanges();
+
+        expect(el.getAttribute('deeplvl')).toEqual('1');
+    });
 });
+
+@Component({
+    selector: 'sdk-test-cmp',
+    template: `
+        <sdk-nav-menu-group>
+            <a sdk-nav-menu-item routerLinkActive="active"
+               routerLink="/guides/fantastic">Home page</a>
+            <a sdk-nav-menu-item routerLinkActive="active"
+               routerLink="/guides/invisible">Get started</a>
+            <a sdk-nav-menu-item routerLinkActive="active" disabled="true"
+               routerLink="/guides/torch">Style guide</a>
+            <a sdk-nav-menu-item routerLinkActive="active" disabled="true"
+               routerLink="/guides/thing">Schematics</a>
+        </sdk-nav-menu-group>`,
+})
+class TestComponent {
+}
