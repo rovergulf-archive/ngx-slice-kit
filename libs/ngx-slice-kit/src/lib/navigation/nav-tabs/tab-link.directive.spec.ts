464433705c224d1fdef1338ced051cc42a993ec7
cosmicintruder <littlegreenbagg@googlemail.com>
2021-02-14 22:59:01 +0300

    test/nav-tab - tablink directive tests;

--- libs/ngx-slice-kit/src/lib/navigation/nav-tabs/tab-link.directive.spec.ts
+++ libs/ngx-slice-kit/src/lib/navigation/nav-tabs/tab-link.directive.spec.ts
@@ -1,8 +1,52 @@
-import { TabLinkDirective } from './tab-link.directive';
+import {TabLinkDirective} from './tab-link.directive';
+import {Component} from '@angular/core';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 
 describe('TabLinkDirective', () => {
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let tablinkDe;
+    let tablinkEl;
+    let directive;
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [TabLinkDirective, TestComponent],
+        });
+
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        tablinkDe = fixture.debugElement.query(By.directive(TabLinkDirective));
+        directive = tablinkDe.injector.get(TabLinkDirective);
+        tablinkEl = tablinkDe.nativeElement;
+
+        fixture.detectChanges();
+    }));
+
     it('should create an instance', () => {
-        const directive = new TabLinkDirective();
         expect(directive).toBeTruthy();
     });
+
+    it('should #active be false by default', () => {
+        expect(directive.active).toBeFalse();
+    });
+
+    it('should #disabled be false by default', () => {
+        expect(directive.disabled).toBeFalse();
+    });
+
+    it('should directive element has default class', () => {
+        const defClass = 'sdk-tab-container__tab';
+        expect(directive.elementClass).toEqual(defClass);
+        expect(tablinkEl).toHaveClass(defClass);
+    });
+
 });
+
+@Component({
+    template: `
+        <a sdkTabLink>test</a>>`
+})
+class TestComponent {
+}
