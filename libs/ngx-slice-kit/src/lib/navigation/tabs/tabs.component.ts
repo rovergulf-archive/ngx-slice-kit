1b19efe8164e1635bc59a133db725316777eebff
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-05 00:02:37 +0300

    bug/tabs - tabscroll bugfix

--- libs/ngx-slice-kit/src/lib/navigation/tabs/tabs.component.ts
+++ libs/ngx-slice-kit/src/lib/navigation/tabs/tabs.component.ts
@@ -11,10 +11,10 @@ import {
     QueryList,
     ViewChild,
 } from '@angular/core';
-import { TabComponent } from './tab/tab.component';
-import { ThemeService } from '../../core/theme/theme.service';
-import { BehaviorSubject, fromEvent, Subscription } from 'rxjs';
-import { delay, throttleTime } from 'rxjs/operators';
+import {TabComponent} from './tab/tab.component';
+import {ThemeService} from '../../core/theme/theme.service';
+import {BehaviorSubject, fromEvent, Subscription} from 'rxjs';
+import {delay, throttleTime} from 'rxjs/operators';
 
 @Component({
     selector: 'sdk-tab-group',
@@ -28,25 +28,24 @@ export class TabsComponent implements OnInit, AfterContentInit, AfterViewInit, A
     @Input() activeTabStyle: string = 'border';
     @Input() animation: boolean = false;
 
-    @ViewChild('container', {static: true}) container;
-    @ViewChild('tabs', {static: true}) tabsContainer;
-    @ViewChild('arrowLeft', {static: true}) arrowLeft;
-    @ViewChild('arrowRight', {static: true}) arrowRight;
+    @ViewChild('container', {static: true}) containerElement;
+    @ViewChild('tabs', {static: true}) tabsWrapperElement;
+    @ViewChild('arrowLeft', {static: true}) arrowLeftElement;
+    @ViewChild('arrowRight', {static: true}) arrowRightElement;
 
     subscription: Subscription = new Subscription();
 
-    tabGroup: TabComponent[] = [];
-    curTab: HTMLElement;
-    containerElement: HTMLElement;
-    curTabClientRect;
-    containerRect;
-    tabsScrollRect;
     tabsViewElements: HTMLElement[];
+    curTab: HTMLElement;
+    tabGroup: TabComponent[] = [];
     containerWidth: number;
     tabsWrapperWidth: number;
     allTabsWidth: number = 0;
     arrowWidth: number = 40;
     isArrows: boolean;
+    curTabClientRect: ClientRect;
+    containerRect: ClientRect;
+    tabsScrollRect: ClientRect;
 
     containerPosition$ = new BehaviorSubject(null);
 
@@ -60,6 +59,56 @@ export class TabsComponent implements OnInit, AfterContentInit, AfterViewInit, A
         private cdRef: ChangeDetectorRef
     ) {
     }
+
+    // selectTab(selectedTab, index): any {
+    //     if (selectedTab.active) {
+    //         return;
+    //     }
+    //
+    //     // set prev tab "hiding" stat and start hide animation for it
+    //     selectedTab.index = index;
+    //     let prevTabIndex: number;
+    //     for (const tab of this.tabGroup) {
+    //         if (tab.active) {
+    //             tab.hiding = true;
+    //             prevTabIndex = tab.index;
+    //             if (this.animation) {
+    //                 tab.slideDirection = tab.index > index ? 'slideRight' : 'slideLeft';
+    //             }
+    //             setTimeout(() => {
+    //                 tab.hiding = false;
+    //             }, this.animation ? 500 : 0);
+    //         }
+    //         tab.active = false;
+    //     }
+    //
+    //     // set new active tab and start show animation
+    //     selectedTab.active = true;
+    //     if (this.animation) {
+    //         selectedTab.slideDirection = selectedTab.index > prevTabIndex ? 'slideLeft' : 'slideRight';
+    //     }
+    //
+    //     setTimeout(() => {
+    //         this.curTab = this.containerElement.querySelector(`.sdk-tab-container__tab--active`);
+    //         this.setUnderlineMeasure();
+    //
+    //         // if current element not fully visible
+    //         if (this.isArrows) {
+    //             if (this.curTabClientRect.left - this.arrowWidth < this.containerRect.left) {
+    //                 const visiblePart = this.curTabClientRect.right - this.arrowWidth - this.containerRect.left;
+    //                 const hiddenPart = this.curTabClientRect.width - visiblePart;
+    //                 const additionalPadding = 40;
+    //                 this.scrollLeft(hiddenPart + additionalPadding);
+    //             }
+    //             if (this.curTabClientRect.right + this.arrowWidth > this.containerRect.right) {
+    //                 const visiblePart = this.containerRect.right - this.arrowWidth - this.curTabClientRect.left;
+    //                 const hiddenPart = this.curTabClientRect.width - visiblePart;
+    //                 const additionalPadding = 40;
+    //                 this.scrollRight(hiddenPart + additionalPadding);
+    //             }
+    //         }
+    //     });
+    // }
 
     selectTab(selectedTab, index): any {
         if (selectedTab.active) {
@@ -91,124 +140,126 @@ export class TabsComponent implements OnInit, AfterContentInit, AfterViewInit, A
 
         setTimeout(() => {
             this.curTab = this.containerElement.querySelector(`.sdk-tab-container__tab--active`);
-            this.setUnderlineMeasure();
+            this.curTabClientRect = this.curTab.getBoundingClientRect();
 
             // if current element not fully visible
             if (this.isArrows) {
-                if (this.curTabClientRect.left - this.arrowWidth < this.containerRect.left) {
+                if ((this.curTabClientRect.left - this.arrowWidth) < this.containerRect.left) {
                     const visiblePart = this.curTabClientRect.right - this.arrowWidth - this.containerRect.left;
                     const hiddenPart = this.curTabClientRect.width - visiblePart;
                     const additionalPadding = 40;
                     this.scrollLeft(hiddenPart + additionalPadding);
                 }
-                if (this.curTabClientRect.right + this.arrowWidth > this.containerRect.right) {
+                if ((this.curTabClientRect.right + this.arrowWidth) > this.containerRect.right) {
                     const visiblePart = this.containerRect.right - this.arrowWidth - this.curTabClientRect.left;
                     const hiddenPart = this.curTabClientRect.width - visiblePart;
                     const additionalPadding = 40;
                     this.scrollRight(hiddenPart + additionalPadding);
                 }
             }
+            this.setUnderlineMeasure();
         });
     }
 
     scrollRight(step = null): void {
-        let x = this.tabsContainer.nativeElement.style.left || 0;
+        let x: number = parseInt(this.tabsWrapperElement.style.left, 10) || 0;
         const defaultStep = this.containerRect.width / 100 * 30;
         let scrollStep = defaultStep;
-        this.tabsScrollRect = this.tabsContainer.nativeElement.getBoundingClientRect();
+        this.tabsScrollRect = this.tabsWrapperElement.getBoundingClientRect();
 
         if (this.tabsScrollRect.right - scrollStep < this.containerRect.right + this.arrowWidth) {
             scrollStep = (this.tabsScrollRect.right - this.containerRect.right) + this.arrowWidth;
-            x = parseInt(x, 10) - scrollStep;
         } else {
             scrollStep = step ? step : defaultStep;
-            x = parseInt(x, 10) - scrollStep;
         }
 
-        this.slideMeasure.left = `${parseInt(this.slideMeasure.left, 10) - scrollStep}px`;
+        x -= scrollStep;
+
         this.moveContainer(x);
     }
 
     scrollLeft(step = null): void {
-        let x = this.tabsContainer.nativeElement.style.left || 0;
+        let x: number = parseInt(this.tabsWrapperElement.style.left, 10) || 0;
         const defaultStep = this.containerRect.width / 100 * 30;
         let scrollStep = defaultStep;
-        this.tabsScrollRect = this.tabsContainer.nativeElement.getBoundingClientRect();
+        this.tabsScrollRect = this.tabsWrapperElement.getBoundingClientRect();
 
         if (this.tabsScrollRect.left + scrollStep > this.containerRect.left + this.arrowWidth) {
-            scrollStep = this.containerRect.left - (this.tabsScrollRect.left - this.arrowWidth);
             x = 0;
         } else {
             scrollStep = step ? step : defaultStep;
-            x = parseInt(x, 10) + scrollStep;
+            x += scrollStep;
         }
 
-        this.slideMeasure.left = `${parseInt(this.slideMeasure.left, 10) + scrollStep}px`;
         this.moveContainer(x);
     }
 
     moveContainer(x): void {
-        this.tabsContainer.nativeElement.style.left = x + 'px';
+        let newX: number = x;
+        if (Math.abs(x) + this.containerWidth - (this.arrowLeftElement.offsetWidth * 2) > this.allTabsWidth) {
+            newX = this.containerWidth - (this.arrowLeftElement.offsetWidth * 2) - this.allTabsWidth;
+        }
+        this.tabsWrapperElement.style.left = newX + 'px';
         this.containerPosition$.next(true);
     }
 
-    changeRects(): void {
-        this.tabsScrollRect = this.tabsContainer.nativeElement.getBoundingClientRect();
+    setUnderlineMeasure(): void {
         this.curTabClientRect = this.curTab.getBoundingClientRect();
+        this.slideMeasure.width = `${this.curTabClientRect.width}px`;
+        this.slideMeasure.left = `${this.curTab.offsetLeft}px`;
     }
 
-    setContainerSizes(): void {
+    setSizes(): void {
         this.containerRect = this.containerElement.getBoundingClientRect();
         this.containerWidth = this.containerRect.width;
-        this.tabsWrapperWidth = this.containerWidth - (this.arrowWidth * 2);
+        this.tabsWrapperWidth = this.containerWidth - (this.isArrows ? (this.arrowWidth * 2) : 0);
     }
 
     setTabSizes(): void {
+        this.allTabsWidth = 0;
         this.tabsViewElements.forEach(tab => {
-            const tabWidth = tab.getBoundingClientRect().width;
+            const tabWidth = tab.offsetWidth;
             this.allTabsWidth += tabWidth;
             if (tabWidth > this.tabsWrapperWidth) {
                 tab.classList.add('sdk-tab-container__tab--oversize');
             }
         });
 
-        this.tabsScrollRect = this.tabsContainer.nativeElement.getBoundingClientRect();
+        this.tabsScrollRect = this.tabsWrapperElement.getBoundingClientRect();
     }
 
-    setUnderlineMeasure(): void {
-        // this.curTabClientRect = this.curTab.getBoundingClientRect();
-        this.slideMeasure.width = `${this.curTab.offsetWidth}px`;
-        const tabsContainerOffset = (parseFloat(this.tabsContainer.nativeElement.style.left) || 0);
-        // this.slideMeasure.left = `${this.curTabClientRect.x - this.containerRect.x - (this.isArrows ? this.arrowWidth : 0)}px`;
-        this.slideMeasure.left = `${this.curTab.offsetLeft + tabsContainerOffset}px`;
-
-        console.log(this.slideMeasure, parseFloat(this.tabsContainer.nativeElement.style.left) || 0, 'rect');
+    changeRects(): void {
+        this.tabsScrollRect = this.tabsWrapperElement.getBoundingClientRect();
+        this.curTabClientRect = this.curTab.getBoundingClientRect();
     }
 
     ngOnInit(): void {
-        this.containerElement = this.container.nativeElement;
-        this.setContainerSizes();
+        this.containerElement = this.containerElement.nativeElement || this.containerElement;
+        this.tabsWrapperElement = this.tabsWrapperElement.nativeElement || this.tabsWrapperElement;
+        this.arrowLeftElement = this.arrowLeftElement.nativeElement || this.arrowLeftElement;
+        this.arrowRightElement = this.arrowRightElement.nativeElement || this.arrowRightElement;
+        this.setSizes();
 
         this.subscription = this.containerPosition$.pipe(delay(400)).subscribe(() => this.changeRects());
 
         const subResize = fromEvent(window, 'resize')
             .subscribe(() => {
-                this.setContainerSizes();
+                this.setSizes();
                 this.isArrows = this.allTabsWidth > this.containerWidth;
                 if (!this.isArrows) {
-                    const x = Math.abs(parseFloat(this.tabsContainer.nativeElement.style.left)) || 0;
-                    this.tabsContainer.nativeElement.style.left = 0 + 'px';
+                    const x = Math.abs(parseFloat(this.tabsWrapperElement.style.left)) || 0;
+                    this.tabsWrapperElement.style.left = '0px';
                     if (x !== 0) {
-                        this.slideMeasure.left = parseFloat(this.slideMeasure.left) + x + 'px';
+                        this.slideMeasure.left = `${parseFloat(this.slideMeasure.left) + x}px`;
                     }
                 }
             });
-        const subRightArrow = fromEvent(this.arrowRight.nativeElement, 'click')
+        const subRightArrow = fromEvent(this.arrowRightElement, 'click')
             .pipe(throttleTime(500))
             .subscribe(() => {
                 this.scrollRight();
             });
-        const subLeftArrow = fromEvent(this.arrowLeft.nativeElement, 'click')
+        const subLeftArrow = fromEvent(this.arrowLeftElement, 'click')
             .pipe(throttleTime(500))
             .subscribe(() => {
                 this.scrollLeft();
@@ -220,13 +271,13 @@ export class TabsComponent implements OnInit, AfterContentInit, AfterViewInit, A
     }
 
     ngAfterContentInit(): void {
+        this.tabGroup = [];
         this.tabs.forEach(tabInstance => this.tabGroup.push(tabInstance));
-        console.log(this.tabGroup, 'dis ins');
         this.selectTab(this.tabs.first, 0);
     }
 
     ngAfterViewInit(): void {
-        this.tabsViewElements = Array.from(this.tabsContainer.nativeElement.children);
+        this.tabsViewElements = Array.from(this.tabsWrapperElement.children);
         this.setTabSizes();
         this.isArrows = this.allTabsWidth > this.containerWidth;
     }
