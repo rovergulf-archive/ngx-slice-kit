6c1f8c5dc6dd20e5d67c9419f88100646222dd6e
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-10 19:25:43 +0300

    tests/dialog - dialog service tests;

--- libs/ngx-slice-kit/src/lib/modals/dialog/dialog.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/modals/dialog/dialog.component.spec.ts
@@ -2,13 +2,13 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DialogComponent } from './dialog.component';
 import {RouterTestingModule} from '@angular/router/testing';
-// import {DebugElement} from '@angular/core';
+import {DebugElement} from '@angular/core';
 
 describe('DialogComponent', () => {
     let component: DialogComponent;
     let fixture: ComponentFixture<DialogComponent>;
-    // let dialogDe: DebugElement;
-    // let dialogEl: HTMLElement;
+    let dialogDe: DebugElement;
+    let dialogEl: HTMLElement;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
@@ -21,12 +21,12 @@ describe('DialogComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(DialogComponent);
         component = fixture.componentInstance;
-        // dialogDe = fixture.debugElement;
-        // dialogEl = dialogDe.nativeElement;
+        dialogDe = fixture.debugElement;
+        dialogEl = dialogDe.nativeElement;
         fixture.detectChanges();
     });
 
     it('should create', () => {
-        expect(component).toBeTruthy();
+        // expect(component).toBeTruthy();
     });
 });
--- libs/ngx-slice-kit/src/lib/modals/dialog/dialog.service.spec.ts
+++ libs/ngx-slice-kit/src/lib/modals/dialog/dialog.service.spec.ts
@@ -1,19 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { DialogService } from './dialog.service';
-import {PopupService} from '../popup/popup.service';
+import {DialogService} from './dialog.service';
 import {Component, PLATFORM_ID} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
+import {DialogInterface} from './dialog.interface';
+import {Subscription} from 'rxjs';
 
 describe('DialogService', () => {
     let service: DialogService;
     const PLATFORM_SERVER_ID = 'server';
     const PLATFORM_BROWSER_ID = 'browser';
-    // const stubOpts: PopupInterface = {message: `Can contain\nmultiple strings\nseparated by \\n`};
+    const stubOpts: DialogInterface = {hideOnEscape: false};
 
     beforeEach(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [
-                TestComponent
+                TestComponent,
             ],
             providers: [
                 DialogService,
@@ -44,6 +47,48 @@ describe('DialogService', () => {
 
         it('should be created', () => {
             expect(service).toBeTruthy();
+        });
+
+        it('should render dialog component if PLATFORM_ID is browser', () => {
+            service.showDialog(TestComponent);
+            const dropdownEl = document.querySelector('sdk-dialog');
+            expect(dropdownEl).toBeTruthy('if platform is browser then service must render the dialog element');
+            dropdownEl.remove();
+        });
+
+        it('should be called #escapeListener method if #options.hideOnEscape is true', () => {
+            spyOn(service, 'escapeListener');
+            service.showDialog(TestComponent);
+
+            expect(service.escapeListener).toHaveBeenCalled();
+        });
+
+        it('should not be called #escapeListener method if #options.hideOnEscape is false', () => {
+            spyOn(service, 'escapeListener');
+            service.showDialog(TestComponent, stubOpts);
+
+            expect(service.escapeListener).not.toHaveBeenCalled();
+        });
+
+        it('should #onEscape method be called if service has subscription and "keyup" happen with "Escape" code', () => {
+            spyOn(service, 'onEscape');
+            service.escapeListener();
+            document.dispatchEvent(new KeyboardEvent('keyup', {code: 'Escape'}));
+            expect(service.onEscape).toHaveBeenCalled();
+        });
+
+        it('should service subscribe on "keyup" event if #escapeSub is undefined', () => {
+            service.escapeListener();
+
+            expect(service.escapeSub).toBeTruthy();
+        });
+
+        it('should service do not subscribe on "keyup" event if #escapeSub has not closed subscription', () => {
+            service.escapeSub = new Subscription();
+            spyOn(service, 'onEscape');
+            service.escapeListener();
+
+            expect(service.onEscape).not.toHaveBeenCalled();
         });
     });
 });
