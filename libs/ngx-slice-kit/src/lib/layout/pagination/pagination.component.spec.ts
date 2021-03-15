f8d73cf8454b63ead357ac9588eb168264d88049
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-15 18:30:44 +0300

    test/pagination - start;

--- libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
@@ -2,10 +2,13 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PaginationComponent } from './pagination.component';
 import {IconComponent} from '../../buttons/icon/icon.component';
+import {DebugElement} from '@angular/core';
 
 describe('PaginationComponent', () => {
     let component: PaginationComponent;
     let fixture: ComponentFixture<PaginationComponent>;
+    let pagDe: DebugElement;
+    let pagEl: HTMLElement;
 
     beforeEach(waitForAsync(() => {
         TestBed.configureTestingModule({
@@ -20,6 +23,8 @@ describe('PaginationComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(PaginationComponent);
         component = fixture.componentInstance;
+        pagDe = fixture.debugElement;
+        pagEl = pagDe.nativeElement;
         fixture.detectChanges();
     });
 
