7ebdcfe7f9dd539e65950375fe57b40f3c834c5c
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-15 23:30:36 +0300

    test/pagination - done;

--- libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
@@ -77,36 +77,308 @@ describe('PaginationComponent', () => {
         expect(el).toHaveClass('sdk-paginator--small');
     });
 
-    // it('should', () => {});
+    it('should #mouseenter/mouseleave events works correctly on left/right buttons', () => {
+        const eventEnter = new MouseEvent('mouseenter');
+        const eventLeave = new MouseEvent('mouseleave');
+        const elLeft: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left');
+        const elRight: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right');
 
-    // it('should', () => {});
+        elLeft.dispatchEvent(eventEnter);
+        expect(component.leftArrowHovered).toBeTrue();
+        elLeft.dispatchEvent(eventLeave);
+        expect(component.leftArrowHovered).toBeFalse();
 
-    // it('should', () => {});
+        elRight.dispatchEvent(eventEnter);
+        expect(component.rightArrowHovered).toBeTrue();
+        elRight.dispatchEvent(eventLeave);
+        expect(component.rightArrowHovered).toBeFalse();
+    });
 
-    // it('should', () => {});
+    it('should left/right button icons have primary color if they are not hovered', () => {
+        component.rightArrowHovered = false;
+        component.leftArrowHovered = false;
+        fixture.detectChanges();
+        const elLeft: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left sdk-icon');
+        const elRight: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right sdk-icon');
+        const attrLeft = elLeft.getAttribute('ng-reflect-color');
+        const attrRight = elRight.getAttribute('ng-reflect-color');
 
-    // it('should', () => {});
+        expect(attrLeft).toEqual('var(--primary)');
+        expect(attrRight).toEqual('var(--primary)');
+    });
 
-    // it('should', () => {});
+    it('should left/right button icons have #fff color if they are hovered', () => {
+        component.rightArrowHovered = true;
+        component.leftArrowHovered = true;
+        fixture.detectChanges();
+        const elLeft: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left sdk-icon');
+        const elRight: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right sdk-icon');
+        const attrLeft = elLeft.getAttribute('ng-reflect-color');
+        const attrRight = elRight.getAttribute('ng-reflect-color');
 
-    // it('should', () => {});
+        expect(attrLeft).toEqual('#fff');
+        expect(attrRight).toEqual('#fff');
+    });
 
-    // it('should', () => {});
+    it('should #pageChange do nothing if page argument is equal current #page property', () => {
+        spyOn(component, 'createPageArray');
+        spyOn(component.changed, 'emit');
+        const dummyPage = 5;
+        component.page = dummyPage;
 
-    // it('should', () => {});
+        fixture.detectChanges();
+        component.pageChange(dummyPage);
+        expect(component.createPageArray).not.toHaveBeenCalled();
+        expect(component.changed.emit).not.toHaveBeenCalled();
+    });
 
-    // it('should', () => {});
+    it('should #pageChange set #page property, emit #changed event and call #createPageArray method if page argument is not equal #page property', () => {
+        spyOn(component, 'createPageArray');
+        spyOn(component.changed, 'emit');
+        const dummyPage = 5;
 
-    // it('should', () => {});
+        fixture.detectChanges();
+        component.pageChange(dummyPage);
+        expect(component.page).toEqual(dummyPage);
+        expect(component.createPageArray).toHaveBeenCalled();
+        expect(component.changed.emit).toHaveBeenCalled();
+    });
 
-    // it('should', () => {});
+    it('should #getPages() return correct page array if count of pages <= 5', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 1;
 
-    // it('should', () => {});
+        component.ngOnInit();
+        fixture.detectChanges();
+        expect(component.getPages()).toEqual([{value: 1}, {value: 2}, {value: 3}]);
+    });
 
-    // it('should', () => {});
+    it('should count of page elements be equal #pageCount', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 1;
+        component.ngOnInit();
+        fixture.detectChanges();
+        const els: NodeListOf<HTMLElement> = pagEl.querySelectorAll('.sdk-paginator__page .sdk-paginator__btn');
 
-    // it('should', () => {});
+        expect(els.length).toEqual(3); // 15 / 5
+    });
 
-    // it('should', () => {});
+    it('should right button be disabled if last page is active', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 3;
+        component.ngOnInit();
+        fixture.detectChanges();
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right');
 
+        expect(el.hasAttribute('disabled')).toBeTrue();
+    });
+
+    it('should left button be disabled if first page is active', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 1;
+        component.ngOnInit();
+        fixture.detectChanges();
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left');
+
+        expect(el.hasAttribute('disabled')).toBeTrue();
+    });
+
+    it('should right button not be disabled if last page is not active', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 2;
+        component.ngOnInit();
+        fixture.detectChanges();
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right');
+
+        expect(el.hasAttribute('disabled')).not.toBeTrue();
+    });
+
+    it('should left button not be disabled if first page is not active', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 2;
+        component.ngOnInit();
+        fixture.detectChanges();
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left');
+
+        expect(el.hasAttribute('disabled')).not.toBeTrue();
+    });
+
+    it('should click on left arrow button call #pageChange method with (page - 1) as value', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 2;
+        component.ngOnInit();
+        fixture.detectChanges();
+        spyOn(component, 'pageChange');
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--left');
+
+        el.click();
+
+        expect(component.pageChange).toHaveBeenCalledWith(1);
+    });
+
+    it('should click on right arrow button call #pageChange method with (page + 1) as value', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 2;
+        component.ngOnInit();
+        fixture.detectChanges();
+        spyOn(component, 'pageChange');
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator__arrow--right');
+
+        el.click();
+
+        expect(component.pageChange).toHaveBeenCalledWith(3);
+    });
+
+    it('should click on page element call #pageChange method with page value as argument', () => {
+        component.limit = 5;
+        component.count = 15;
+        component.page = 2;
+        component.ngOnInit();
+        fixture.detectChanges();
+        spyOn(component, 'pageChange');
+        const el: HTMLElement = pagEl.querySelectorAll('.sdk-paginator__page .sdk-paginator__btn')[1] as HTMLElement; // second page
+
+        el.click();
+
+        expect(component.pageChange).toHaveBeenCalledWith(2);
+    });
+
+    describe('#createPageArray() tests', () => {
+        const limit = 5;
+        const itemCount = 50;
+        const pageCount = Math.ceil(itemCount / limit);
+        const page = 6;
+        const dummyA = [
+            {value: 1},
+            {value: 2},
+            {value: 3},
+            {value: '...', disabled: true},
+            {value: pageCount}
+        ];
+        const dummyB = [
+            {value: 1},
+            {value: 2},
+            {value: 3},
+            {value: 4},
+            {value: '...', disabled: true},
+            {value: pageCount}
+        ];
+        const dummyC = [
+            {value: 1},
+            {value: 2},
+            {value: 3},
+            {value: 4},
+            {value: '...', disabled: true},
+            {value: pageCount}
+        ];
+        const dummyD = [
+            {value: 1},
+            {value: '...', disabled: true},
+            {value: pageCount - 3},
+            {value: pageCount - 2},
+            {value: pageCount - 1},
+            {value: pageCount}
+        ];
+        const dummyE = [
+            {value: 1},
+            {value: '...', disabled: true},
+            {value: pageCount - 3},
+            {value: pageCount - 2},
+            {value: pageCount - 1},
+            {value: pageCount}
+        ];
+        const dummyF = [
+            {value: 1},
+            {value: '...', disabled: true},
+            {value: pageCount - 2},
+            {value: pageCount - 1},
+            {value: pageCount}
+        ];
+        const dummyG = [
+            {value: 1},
+            {value: '...', disabled: true},
+            {value: page - 1},
+            {value: page},
+            {value: page + 1},
+            {value: '...', disabled: true},
+            {value: pageCount}
+        ];
+
+        beforeEach(() => {
+            component.limit = 5;
+            component.count = 50;
+        });
+
+        it('test pagination case #1', () => {
+            component.page = 1;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyA);
+        });
+
+        it('test pagination case #2', () => {
+            component.page = 2;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyB);
+        });
+
+        it('test pagination case #3', () => {
+            component.page = 3;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyC);
+        });
+
+        it('test pagination case #4', () => {
+            component.page = pageCount - 2;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyD);
+        });
+
+        it('test pagination case #5', () => {
+            component.page = pageCount - 1;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyE);
+        });
+
+        it('test pagination case #6', () => {
+            component.page = pageCount;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyF);
+        });
+
+        it('test pagination case #7', () => {
+            component.page = page;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.createPageArray()).toEqual(dummyG);
+        });
+
+        it('should #getPages() return correct page array', () => {
+            component.page = 1;
+            component.ngOnInit();
+            fixture.detectChanges();
+
+            expect(component.getPages()).toEqual(dummyA);
+        });
+    });
 });
