3035085aa7e2036488ae662e54164f10d156a3dd
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-15 19:22:59 +0300

    test/pagination - test pack;

--- libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/layout/pagination/pagination.component.spec.ts
@@ -31,4 +31,82 @@ describe('PaginationComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should #getPageCount return (count of items / items per page)', () => {
+        component.count = 20;
+        component.limit = 5;
+        fixture.detectChanges();
+        const expectedPageCount = component.count / component.limit; // 4;
+        const res = component.getPageCount();
+        fixture.detectChanges();
+
+        expect(res).toEqual(expectedPageCount);
+    });
+
+    it('should #ngOnInit call #getPageCount method to set #pageCount', () => {
+        spyOn(component, 'getPageCount');
+        component.ngOnInit();
+
+        expect(component.getPageCount).toHaveBeenCalled();
+    });
+
+    it('should #pageCount be equal (count of items / items per page) after component was init', () => {
+        component.count = 20;
+        component.limit = 5;
+        fixture.detectChanges();
+        const expectedPageCount = component.count / component.limit; // 4;
+        component.ngOnInit();
+        fixture.detectChanges();
+
+        expect(component.pageCount).toEqual(expectedPageCount);
+    });
+
+    it('should #small be false by default and element has no "small" class', () => {
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator');
+
+        expect(el).toHaveClass('sdk-paginator--default');
+        expect(el).not.toHaveClass('sdk-paginator--small');
+    });
+
+    it('should element has .sdk-paginator--small class if #small is true', () => {
+        component.small = true;
+        fixture.detectChanges();
+        const el: HTMLElement = pagEl.querySelector('.sdk-paginator');
+
+        expect(el).not.toHaveClass('sdk-paginator--default');
+        expect(el).toHaveClass('sdk-paginator--small');
+    });
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
 });
