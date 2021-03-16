7aa2c569c278b1dde2de459744ccec4085622075
Dmitrii Limonov <disordervlz@gmail.com>
2021-03-16 17:21:05 +0300

    use theme props to form color palette

--- libs/ngx-slice-kit/src/lib/core/theme/lib/theme-dark.ts
+++ libs/ngx-slice-kit/src/lib/core/theme/lib/theme-dark.ts
@@ -1,10 +1,14 @@
 import { defaultAccentRgb, defaultPrimaryRgb, defaultSuccessRgb, defaultWarnRgb, Theme } from '../theme.model';
 
+export const defaultBaseRgb = `26, 26, 26`;
+export const defaultOppositeRgb = `255, 255, 255`;
+export const defaultRegularRgb = `186, 186, 186`;
+
 export const themeDark: Theme = {
     name: 'dark',
-    baseRgb: `26, 26, 26`,
-    oppositeRgb: `255, 255, 255`,
-    regularRgb: `186, 186, 186`,
+    baseRgb: defaultBaseRgb,
+    oppositeRgb: defaultOppositeRgb,
+    regularRgb: defaultRegularRgb,
     primaryRgb: defaultPrimaryRgb,
     successRgb: defaultSuccessRgb,
     accentRgb: defaultAccentRgb,
--- libs/ngx-slice-kit/src/lib/core/theme/theme.model.ts
+++ libs/ngx-slice-kit/src/lib/core/theme/theme.model.ts
@@ -13,9 +13,56 @@ export const colors = [
 export const emptyDepth = ``;
 export const rgbDepth = `rgb`;
 
-export const colorsDepth = [
-    `rgb`, `deep`, `hover`, `active`, `disabled`, `smooth`, `placeholder`, `text`, `text-reverse`
-];
+export const colorsDepth = {
+    a100: 'rgb',
+    a80: 'deep',
+    a95: 'hover',
+    a75: 'active',
+    a50: 'disabled',
+    a10: 'smooth',
+};
+
+export const textDepth = {
+    'a-40': 'placeholder',
+    'a-80': 'text',
+    a100: 'text-reverse',
+};
+
+// '--regular-rgb': '95,95,95',
+//     '--regular-deep': 'rgb(66,66,66)',
+//     '--regular': 'rgb(95,95,95)', // rgba(26, 26, 26, 0.70)
+//     '--regular-hover': 'rgb(163, 163, 163)', // rgba(26, 26, 26, 0.40)
+//     '--regular-active': 'rgb(200, 200, 200)', // rgba(26, 26, 26, 0.24)
+//     '--regular-disabled': 'rgb(233, 233, 233)', // rgba(26, 26, 26, 0.1)
+//     '--regular-smooth': 'rgb(246, 246, 246)', // rgba(26, 26, 26, 0.1)
+//     '--regular-placeholder': 'rgb(163, 163, 163)',
+//     '--regular-text': 'rgb(var(--opposite-rgb))',
+//     '--regular-text-reverse': 'rgb(var(--base-rgb))',
+
+class ColorProperty {
+    name: string;
+    value: string;
+
+    constructor(cp: ColorProperty) {
+        Object.assign(this, cp);
+    }
+
+    set base(color: string) {
+        this.value = color;
+    }
+
+    get rgb(): string {
+        return this.base ? `var(--${this.base}-rgb)` : this.value;
+    }
+
+    get hex(): string {
+        return ``;
+    }
+
+    alpha(): string {
+        return '';
+    }
+}
 
 export class Theme {
     name: string;
@@ -62,27 +109,62 @@ export class Theme {
         }
     }
 
-    public props?(): { name: string, value: string }[] {
+    public props?(): ColorProperty[] {
         const results = [];
 
         for (const color of colors) {
             const cKey = `${color}Rgb`;
             const rgb = this[cKey];
             results.push({
-                name: `--${color}`,
+                name: `--${color}-rgb`,
                 value: rgb
             });
 
-            for (const d of colorsDepth) {
-                const depth = `--${color}-${d}`;
-                const dRgb = `${rgb},`;
-                results.push({
-                    name: depth,
-                    value: dRgb,
-                });
+            for (let ind = 100; ind >= 0; ind--) {
+                if (ind % 5 === 0) {
+                    const alpha = `a${ind}`;
+                    results.push({
+                        name: `--${color}-${alpha}`,
+                        value: `rgba(rgb, ${ind})`
+                    });
+                }
+            }
+
+            if (colors.indexOf(color) > 1) {
+                results.concat(results, objectToColorProperties(colorsDepth, color));
+                results.concat(results, objectToColorProperties(textDepth, color));
             }
         }
 
+        results.push({
+            name: '--background',
+            value: 'rgb(var(--opposite-rgb))'
+        }, {
+            name: '--color',
+            value: 'var(--regular-text)'
+        });
+
         return results;
     }
+
 }
+
+const objectToColorProperties = (srcObj: any, color: string): ColorProperty[] => {
+    const results: ColorProperty[] = [];
+
+    for (const key of Object.keys(srcObj)) {
+        if (srcObj.hasOwnProperty(key)) {
+            const keyName = srcObj[key];
+            const alpha = parseInt(key.substring(1), 10) / 100;
+            const colorVar = alpha > 0 ? 'var(--base-rgb)' : 'var(--opposite-rgb)';
+            const prop = {
+                name: `--${color}-${keyName}`,
+                value: `var(--${color}-${key})`
+            };
+            console.log(prop);
+            results.push(new ColorProperty(prop as ColorProperty));
+        }
+    }
+
+    return results;
+};
