b5485599aee2ac777c2a353d1109e02b22571593
Dmitrii Limonov <disordervlz@gmail.com>
2021-03-16 15:15:50 +0300

    separate media module as part of #31

--- libs/ngx-slice-kit/src/lib/media/media.module.ts
+++ libs/ngx-slice-kit/src/lib/media/media.module.ts
@@ -1,25 +1,24 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import {AudioPlayerComponent} from './audio-player/audio-player.component';
-// import {VideoPlayerComponent} from './video-player/video-player.component';
-// import {FileComponent} from './file/file.component';
+import { AudioPlayerModule } from './audio-player/audio-player.module';
+import { FileModule } from './file/file.module';
+import { VideoPlayerModule } from './video-player/video-player.module';
 
-const moduleDirectives = [];
-
-const moduleDeclarations = [
-    ...moduleDirectives,
-    // AudioPlayerComponent,
-    // FileComponent,
-    // VideoPlayerComponent,
+const modules = [
+    AudioPlayerModule,
+    FileModule,
+    VideoPlayerModule,
 ];
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: moduleDeclarations,
-    exports: moduleDeclarations,
+    declarations: [],
+    exports: [
+        ...modules,
+    ],
     providers: [],
     schemas: [
         CUSTOM_ELEMENTS_SCHEMA,
