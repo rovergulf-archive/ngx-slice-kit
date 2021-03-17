7daf4a861458c1afc9933f91528143c60f89d7a1
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-17 23:48:35 +0300

    tests-fixes - input fixes#3;

--- libs/ngx-slice-kit/src/lib/inputs/input/input.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/inputs/input/input.component.spec.ts
@@ -314,13 +314,14 @@ describe('InputComponent', () => {
         expect(eventBody.target).toEqual(expectedEventBody.target);
     });
 
-    it('should autocomplete set #focused as true and add focused classes', fakeAsync(() => {
+    it('should autocomplete set #focused as true and add focused classes', () => {
         component.autofocus = true;
+        fixture.detectChanges();
         component.ngOnInit();
         component.ngAfterContentInit();
+        component.ngAfterViewInit();
         fixture.detectChanges();
 
-        tick(1000);
 
         const label: HTMLElement = input.querySelector('.sdk-input__label');
         const wrapper: HTMLElement = input.querySelector('.sdk-input-wrap');
@@ -328,7 +329,7 @@ describe('InputComponent', () => {
         expect(component.focused).toEqual(true, 'should be focused after #focus()');
         expect(label).toHaveClass('sdk-input__label--focused');
         expect(wrapper).toHaveClass('sdk-input-wrap--focused');
-    }));
+    });
 });
 
 // InputComponent test wrapper
