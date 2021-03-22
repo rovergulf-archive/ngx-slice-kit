49892d1d12804912e8b94b54b3a2b581affa0e14
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-22 23:03:42 +0300

    i17r.pipe test change;

--- libs/ngx-slice-kit/src/lib/core/i17r/i17r.pipe.spec.ts
+++ libs/ngx-slice-kit/src/lib/core/i17r/i17r.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { I17rPipe } from './i17r.pipe';
 import { I17rService } from './i17r.service';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('I17rPipe', () => {
     let service: I17rService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            // imports: [RouterTestingModule],
+            providers: [
+                I17rService,
+            ]
+        });
+        service = TestBed.inject(I17rService);
+    });
+
     it('create an instance', () => {
         const pipe = new I17rPipe(
             service
