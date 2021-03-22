ead90f5564927fe6d3673bc1d03fd86bf9786c96
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-22 23:57:05 +0300

    loading.directive test;

--- libs/ngx-slice-kit/src/lib/layout/loading/loading.directive.spec.ts
+++ libs/ngx-slice-kit/src/lib/layout/loading/loading.directive.spec.ts
@@ -1,14 +1,29 @@
 import { LoadingDirective } from './loading.directive';
-import { ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
+import {ComponentFactoryResolver, ViewContainerRef} from '@angular/core';
+import {TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoadingDirective', () => {
+    let directive: LoadingDirective;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
+            providers: [
+                LoadingDirective,
+                ViewContainerRef,
+                ComponentFactoryResolver
+            ]
+        });
+        directive = TestBed.inject(LoadingDirective);
+    });
+
     it('should create an instance', () => {
-        let viewRef: ViewContainerRef;
-        let resolver: ComponentFactoryResolver;
-        const directive = new LoadingDirective(
-            viewRef,
-            resolver
-        );
+        // let viewRef: ViewContainerRef;
+        // let resolver: ComponentFactoryResolver;
+        // const directive = new LoadingDirective(
+        //     viewRef,
+        //     resolver
+        // );
         expect(directive).toBeTruthy();
     });
 });
