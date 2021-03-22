bf21964db76ce657783a4f8bac5f9312886512b8
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-22 23:58:23 +0300

    theme.directive test;

--- libs/ngx-slice-kit/src/lib/core/theme/theme.directive.spec.ts
+++ libs/ngx-slice-kit/src/lib/core/theme/theme.directive.spec.ts
@@ -1,23 +1,43 @@
 import { ThemeDirective } from './theme.directive';
-import { ElementRef, Renderer2 } from '@angular/core';
 import { ThemeService } from './theme.service';
 import { LayoutControlService } from '../layout-control/layout-control.service';
+import {TestBed} from '@angular/core/testing';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import {ElementRef, Renderer2} from '@angular/core';
 
 describe('ThemeDirective', () => {
     let layoutControl: LayoutControlService;
     let themeService: ThemeService;
 
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [BrowserDynamicTestingModule],
+            providers: [
+                ThemeDirective,
+                Renderer2,
+                {provide: ElementRef, useValue: new MockElementRef({})}
+            ]
+        });
+        layoutControl = TestBed.inject(LayoutControlService);
+        themeService = TestBed.inject(ThemeService);
+    });
+
     it('should create an instance', () => {
-        let document: Document;
-        let el: ElementRef;
-        let renderer: Renderer2;
-        const directive = new ThemeDirective(
-            document,
-            el,
-            renderer,
-            themeService,
-            layoutControl,
-        );
+        // let document: Document;
+        // let el: ElementRef;
+        // let renderer: Renderer2;
+        // const directive = new ThemeDirective(
+        //     document,
+        //     el,
+        //     renderer,
+        //     themeService,
+        //     layoutControl,
+        // );
+        const directive = TestBed.inject(ThemeDirective);
         expect(directive).toBeTruthy();
     });
 });
+
+export class MockElementRef extends ElementRef {
+    nativeElement = {};
+}
