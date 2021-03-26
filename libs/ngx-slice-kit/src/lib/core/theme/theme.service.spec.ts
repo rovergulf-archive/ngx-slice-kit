f14513f116c15cf3d9e86c1bab1e7f9bf099b308
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-26 21:42:48 +0300

    test/theme-service: few tests;

--- libs/ngx-slice-kit/src/lib/core/theme/theme.service.spec.ts
+++ libs/ngx-slice-kit/src/lib/core/theme/theme.service.spec.ts
@@ -15,18 +15,31 @@ describe('ThemeService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('should', () => {
+    it('should #findTheme return correct theme by name', () => {
         const themeName = 'dark';
         const theme = service.findTheme(themeName);
 
         expect(theme.name).toEqual(themeName);
     });
 
-    // it('should', () => {});
+    it('should #getCurrentThemeIndex be 0 by default, because of light theme', () => {
+        expect(service.getCurrentThemeIndex()).toEqual(0);
+    });
 
-    // it('should', () => {});
+    it('should #getCurrentThemeIndex be 1 if was set dark theme ', () => {
+        service.setTheme('dark');
+        expect(service.getCurrentThemeIndex()).toEqual(1);
+    });
 
-    // it('should', () => {});
+    it('should #getDefaultTheme return light theme', () => {
+        expect(service.getDefaultTheme()).toEqual(service.themes[0]);
+    });
+
+    it('should #themeName return correct name', () => {
+        expect(service.themeName).toEqual('light', 'should name of default theme be "light"');
+        service.setTheme('dark');
+        expect(service.themeName).toEqual('dark');
+    });
 
     // it('should', () => {});
 
