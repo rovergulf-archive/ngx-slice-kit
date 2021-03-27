efac234dde3eb7a0b821b275177056f94fb82376
cosmicintruder <littlegreenbagg@googlemail.com>
2021-03-27 20:53:04 +0300

    test/theme-dir: tests pack;

--- libs/ngx-slice-kit/src/lib/core/theme/theme.directive.spec.ts
+++ libs/ngx-slice-kit/src/lib/core/theme/theme.directive.spec.ts
@@ -1,30 +1,72 @@
 import { ThemeDirective } from './theme.directive';
 import { ThemeService } from './theme.service';
 import { LayoutControlService } from '../layout-control/layout-control.service';
-import {TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
-import {ElementRef, Renderer2} from '@angular/core';
+import {Component, DebugElement, ElementRef, OnInit, Renderer2} from '@angular/core';
+
+const renderer2Mock = jasmine.createSpyObj('renderer2Mock', [
+    'destroy',
+    'createElement',
+    'createComment',
+    'createText',
+    'destroyNode',
+    'appendChild',
+    'insertBefore',
+    'removeChild',
+    'selectRootElement',
+    'parentNode',
+    'nextSibling',
+    'setAttribute',
+    'removeAttribute',
+    'addClass',
+    'removeClass',
+    'setStyle',
+    'removeStyle',
+    'setProperty',
+    'setValue',
+    'listen'
+]);
+
+const rootRendererMock =  {
+    createElement: () => {
+        return renderer2Mock;
+    }
+};
 
 describe('ThemeDirective', () => {
     let layoutControl: LayoutControlService;
     let themeService: ThemeService;
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let themeDe: DebugElement;
+    let themeEl: HTMLElement;
+    let directive: ThemeDirective;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [BrowserDynamicTestingModule],
             declarations: [
                 TestComponent,
+                ThemeDirective,
                 // ThemeService,
                 // layoutControl
             ],
             providers: [
                 ThemeDirective,
-                Renderer2,
+                // Renderer2,
+                {provide: Renderer2, useValue: rootRendererMock},
                 {provide: ElementRef, useValue: new MockElementRef({})}
             ]
         });
+
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        themeDe = fixture.debugElement;
+        themeEl = themeDe.nativeElement;
         layoutControl = TestBed.inject(LayoutControlService);
         themeService = TestBed.inject(ThemeService);
+        directive = TestBed.inject(ThemeDirective);
     });
 
     it('should create an instance', () => {
@@ -38,9 +80,69 @@ describe('ThemeDirective', () => {
         //     themeService,
         //     layoutControl,
         // );
-        const directive = TestBed.inject(ThemeDirective);
+
         expect(directive).toBeTruthy();
     });
+
+    it('should create component', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should #scoped be false by default', () => {
+        expect(directive.scoped).toBeFalse();
+    });
+
+    it('should #scoped set correct value from input', () => {
+        directive.scoped = true;
+        expect(directive.scoped).toBeTrue();
+    });
+
+    it('should #getElement return body element if #scoped is false', () => {
+        directive.scoped = false;
+        expect(directive.getElement().tagName).toEqual('BODY');
+    });
+
+    it('should #getElement return directive elementRef if #scoped is true', () => {
+        directive.scoped = true;
+        expect(directive.getElement()).toEqual({});
+    });
+
+    it('should #theme be undefined by default', () => {
+        expect(directive.theme).toBeUndefined();
+    });
+
+    it('should #theme has correct value from input if it was set', () => {
+        directive.theme = 'pinkblue';
+        expect(directive.theme).toEqual('pinkblue');
+    });
+
+    it('should be called #themeService.setTheme if #theme has value after init', () => {
+        const t = 'blue';
+        directive.theme = t;
+        spyOn(themeService, 'setTheme');
+
+        directive.ngOnInit();
+        expect(themeService.setTheme).toHaveBeenCalledWith(t);
+    });
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
+    // it('should', () => {});
+
 });
 
 export class MockElementRef extends ElementRef {
@@ -53,5 +155,8 @@ export class MockElementRef extends ElementRef {
             <p>acca dacca</p>
         </div>>`
 })
-class TestComponent {
+class TestComponent implements OnInit {
+
+    ngOnInit(): void {
+    }
 }
