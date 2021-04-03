a5f194bffcbc09d839258db227053ef4604c1883
cosmicintruder <littlegreenbagg@googlemail.com>
2021-04-04 00:06:00 +0300

    palette fix: test fixes;
    Signed-off-by: cosmicintruder <littlegreenbagg@googlemail.com>

--- libs/ngx-slice-kit/src/lib/buttons/icon/icon.component.spec.ts
+++ libs/ngx-slice-kit/src/lib/buttons/icon/icon.component.spec.ts
@@ -47,7 +47,7 @@ describe('IconComponent', () => {
     });
 
     it('should be #color equal default value', () => {
-        const defaultColor = 'var(--color)';
+        const defaultColor = 'var(--base)';
         expect(component.color).toBe(defaultColor);
     });
 
