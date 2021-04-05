e4e0add4524c349befefa0822b276cd283a6b414
Dmitrii Limonov <disordervlz@gmail.com>
2021-04-05 20:00:33 +0300

    fix github workflows
    Signed-off-by: Dmitrii Limonov <disordervlz@gmail.com>

--- libs/ngx-slice-kit/schematics/ng-add/index.ts
+++ libs/ngx-slice-kit/schematics/ng-add/index.ts
@@ -3,6 +3,10 @@ import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 // Just return the tree
 export function ngAdd(options: any): Rule {
+    if (typeof options !== 'undefined') {
+        console.warn('Unexpected, but it\'s ok');
+    }
+
     return (tree: Tree, context: SchematicContext) => {
         context.addTask(new NodePackageInstallTask());
         return tree;
