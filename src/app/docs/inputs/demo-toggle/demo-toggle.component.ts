3e43a7f51058931bd41ffbbfb557bd0476f43b42
Dmitrii Limonov <disordervlz@gmail.com>
2021-04-06 00:07:58 +0300

    move demo toggle component
    Signed-off-by: Dmitrii Limonov <disordervlz@gmail.com>

---
+++ src/app/docs/inputs/demo-toggle/demo-toggle.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-switches',
+    templateUrl: './demo-toggle.component.html',
+    styleUrls: ['./demo-toggle.component.scss']
+})
+export class DemoToggleComponent implements OnInit, OnDestroy {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    ngOnDestroy(): void {
+    }
+
+}
