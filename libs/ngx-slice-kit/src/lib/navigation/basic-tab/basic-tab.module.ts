fd1126a7353d7d5e724bfabd8048adf5c86a0e0b
cosmicintruder <littlegreenbagg@googlemail.com>
2021-05-17 18:29:32 +0300

    update version in package
    Signed-off-by: cosmicintruder <littlegreenbagg@googlemail.com>

--- libs/ngx-slice-kit/src/lib/navigation/basic-tab/basic-tab.module.ts
+++ libs/ngx-slice-kit/src/lib/navigation/basic-tab/basic-tab.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicTabComponent } from './basic-tab.component';
 import { IconModule } from '../../buttons/icon/icon.module';
@@ -14,6 +14,10 @@ import { IconModule } from '../../buttons/icon/icon.module';
     imports: [
         CommonModule,
         IconModule
+    ],
+    schemas: [
+        CUSTOM_ELEMENTS_SCHEMA,
+        NO_ERRORS_SCHEMA,
     ]
 })
 export class BasicTabModule {
