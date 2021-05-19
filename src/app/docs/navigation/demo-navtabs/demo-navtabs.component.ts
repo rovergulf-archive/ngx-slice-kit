3e57258c87b3a85c9fbf3a3cb5cd314696f97eb9
cosmicintruder <littlegreenbagg@googlemail.com>
2021-05-20 00:04:59 +0300

    start
    Signed-off-by: cosmicintruder <littlegreenbagg@googlemail.com>

--- src/app/docs/navigation/demo-navtabs/demo-navtabs.component.ts
+++ src/app/docs/navigation/demo-navtabs/demo-navtabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DemoPageModel } from '../../../shared/model';
 
 @Component({
     selector: 'app-demo-navtabs',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./demo-navtabs.component.scss', '../../docs.module.scss']
 })
 export class DemoNavtabsComponent implements OnInit {
+
+    @ViewChild('basicRef', {static: true}) basicRef: any;
+
+    page: DemoPageModel;
 
     links = [
         {src: 'demo-first', label: 'Lord of the rings: Return of the King'},
@@ -20,5 +25,70 @@ export class DemoNavtabsComponent implements OnInit {
 
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.page = {
+            title: 'Nav-tabs usage example',
+            subtitle: '',
+            demos: [
+                {
+                    title: 'Nav-tabs',
+                    description: '',
+                    templateRef: this.basicRef,
+                    values: {
+                        html: `<section class="flex-column layout-start-stretch">
+    <h2>Nav tabs</h2>
+    <div class="example flex-column layout-start-stretch">
+        <sdk-nav-tab-group [animation]="true" [minHeight]="1048">
+            <a sdkTabLink *ngFor="let link of links"
+               [disabled]="link.disabled"
+               [routerLink]="link.src"
+               [label]="link.label"></a>
+        </sdk-nav-tab-group>
+    </div>
+</section>`,
+                        module: `import { NavTabsModule } from 'ngx-slice-kit';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        // add your routing module
+        SomeRoutingModule
+        // add NavTabsModule to app imports
+        NavTabsModule,
+    ],
+})
+export class DemoNavTabsModule {
+}`,
+                        component: `import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-navtabs',
+    templateUrl: './demo-navtabs.component.html',
+    styleUrls: ['./demo-navtabs.component.scss']
+})
+export class DemoNavtabsComponent {
+
+    links = [
+        {src: 'demo-first', label: 'Lord of the rings: Return of the King'},
+        {src: 'demo-second', label: 'Tab Number Two', disabled: true},
+        {src: 'demo-third', label: 'Millennium falcon'},
+        {src: 'demo-fourth', label: 'Lord of the rings: The Fellowship of the Ring'},
+        {src: 'demo-fifth', label: 'Lord of the rings: The Two Towers'},
+        {src: 'demo-sixth', label: 'Disabled tab', disabled: true},
+        {src: 'demo-seventh', label: 'The Empire strikes back'},
+        {src: 'demo-eighth', label: 'Double dare you once again'},
+    ];
+
+    constructor() {
+    }
+
+}`,
+                    },
+                }
+            ],
+            apis: [
+            ]
+        };
+    }
 }
--- src/app/docs/navigation/demo-tabs/demo-tabs.component.ts
+++ src/app/docs/navigation/demo-tabs/demo-tabs.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { DemoPageModel } from '../../../shared/model';
 
 @Component({
     selector: 'app-demo-tabs',
@@ -7,9 +8,200 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class DemoTabsComponent implements OnInit, OnDestroy {
 
+    @ViewChild('basicRef', {static: true}) basicRef: any;
+
+    page: DemoPageModel;
+
     constructor() {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.page = {
+            title: 'Tabs usage example',
+            subtitle: '',
+            demos: [
+                {
+                    title: 'Tabs',
+                    description: '',
+                    templateRef: this.basicRef,
+                    values: {
+                        html: `<div>
+    <h1>Tabs</h1>
+    <sdk-tab-group [animation]="true">
+        <sdk-tab label="Tab 1" icon="star">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]">
+                </sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Disabled Tab" [disabled]="true"></sdk-tab>
+        <sdk-tab label="Super-mega-uber-extra-duper-long-tab">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Another-one-super-duper-extra-long-tab">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Test tab">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Next tab">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Tab Tab Tab Tab Tab Tab Tab Tab Tab Tab5" icon="arrow-right">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Tab 2">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick color</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'red'}, {value: 1, name: 'blue'}, {value: 2, name: 'green'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick fruit</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'mango'}, {value: 1, name: 'banana'}, {value: 2, name: 'mandarin'}]"></sdk-radio>
+                <h2 class="demo-tabs__title">Pick drink</h2>
+                <sdk-radio
+                    [data]="[{value: 0, name: 'milkshake'}, {value: 1, name: 'kombucha'}, {value: 2, name: 'smoothie'}]"></sdk-radio>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Tab 2 very long sheeeit">
+            <div class="tab-wrapper">
+                <h2 class="demo-tabs__title">Pick movies</h2>
+                <ul>
+                    <li>
+                        <div>Lord of the rings</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                    <li>
+                        <div>Star wars</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                    <li>
+                        <div>Pulp fiction</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                    <li>
+                        <div>Watchmen</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                    <li>
+                        <div>Joker</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                    <li>
+                        <div>Gladiator</div>
+                        <div>
+                            <sdk-checkbox></sdk-checkbox>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </sdk-tab>
+        <sdk-tab label="Tab 3">
+            <div>
+                <h1>Title 3</h1>
+                Content 3
+            </div>
+        </sdk-tab>
+    </sdk-tab-group>
+</div>`,
+                        module: `import { TabsModule } from 'ngx-slice-kit';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        // add TabsModule to app imports
+        TabsModule,
+    ],
+})
+export class DemoTabsModule {
+}`,
+                        component: `import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-demo-tabs',
+    templateUrl: './demo-tabs.component.html',
+    styleUrls: ['./demo-tabs.component.scss']
+})
+export class DemoTabsComponent {
+
+    constructor() {
+    }
+
+}`,
+                    },
+                }
+            ],
+            apis: [
+            ]
+        };
+    }
 
     ngOnDestroy(): void {}
 }
